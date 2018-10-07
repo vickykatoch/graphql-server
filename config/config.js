0936b7d93f63fe11d418f4fa86f4c370b2b18e0b
Balwinder Katoch <balwinderkatoch@MacBook-Pro.local>
2018-10-07 09:23:52 -0400

    user mutations

--- config/config.js
+++ config/config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     dbType: 'sqlite',
     dbPath : path.join(__dirname,'../data/database/app-database.db'),
+    // dbPath : ":memory:",
     port : 5000,
     instances : 4
 };
